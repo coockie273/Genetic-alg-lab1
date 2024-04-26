f8a2bf733721b062e5d8ce9289ecfce5684966d6
Fedotov Stanislav <103525603+coockie273@users.noreply.github.com>
2024-04-26 21:43:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from Population import Population
+from ExtremumFinder import ExtremumFinder
 
 
 def func(x):
     return math.sin(x) / (1 + math.exp(-x))
 
 
-print(Population(100, 1000, 0.5, 0.01, func).start())
+print(ExtremumFinder(100, 1000, 0.5, 0.01, func).start())
